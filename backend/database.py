a2225645306f54b0d116caaaa81dd55788865b91
Sussu Chocolates <sussuchocolates@hotmail.com>
2026-04-27 17:37:05 +0000

    Fix TLS connection to Atlas

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 """
-Database configuration and connection - usando pymongo async nativo
+Database configuration and connection
 """
 from pymongo import AsyncMongoClient
 from dotenv import load_dotenv
@@ -12,5 +12,5 @@ load_dotenv(ROOT_DIR / '.env')
 MONGO_URL = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
 DB_NAME = os.environ.get('DB_NAME', 'sussu_chocolates_db')
 
-client = AsyncMongoClient(MONGO_URL)
+client = AsyncMongoClient(MONGO_URL, tls=True, tlsAllowInvalidCertificates=False)
 db = client[DB_NAME]
